49716df1899fd7be187f0209c5f11392525089dc
Alexandre Silva <xande1231231@hotmail.com>
2018-04-15 13:46:06 -0300

    Fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <div class="container">
     <h1>Animes</h1>
 
-        <div id="coveranime">
+            <div id="coveranime">
                 <img src="/img/animes/record.jpeg">
                 <div id="footera">
                     <p>Record of Grancrest ..</p>
@@ -44,6 +44,42 @@
                     <p>One Punch Man</p>
                 </div>
             </div>
+
+<div id="coveranime">
+                <img src="/img/animes/record.jpeg">
+                <div id="footera">
+                    <p>Record of Grancrest ..</p>
+                </div>
+            </div>
+
+             <div id="coveranime">
+                <img src="/img/animes/capa.jpg">
+                <div id="footera">
+                    <p>One Punch Man</p>
+                </div>
+            </div>
+
+             <div id="coveranime">
+                <img src="/img/animes/rezero.jpg">
+                <div id="footera">
+                    <p>Re:Zero</p>
+                </div>
+            </div>
+
+            <div id="coveranime">
+                <img src="/img/animes/rezero.jpg">
+                <div id="footera">
+                    <p>Re:Zero</p>
+                </div>
+            </div>
+
+             <div id="coveranime">
+                <img src="/img/animes/capa.jpg">
+                <div id="footera">
+                    <p>One Punch Man</p>
+                </div>
+            </div>
+
         </div>
 
     </div>
