a710edb69a26c53cfad3b0e3cef6eb355817111c
Alexandre Silva <xande1231231@hotmail.com>
2018-04-15 13:41:31 -0300

    Fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,52 @@
 
 
     <div class="animes-destaque">
-        <div class="logo">
-            <img src="https://dailyanimeart.files.wordpress.com/2016/09/one-punch-man-logo.png">
+    </div>
+
+    <div class="sectionh animes">
+    <div class="container">
+    <h1>Animes</h1>
+
+    <div class="scroll">
+        <div id="scroller">
+        <div id="coveranime">
+                <img src="/img/animes/record.jpeg">
+                <div id="footera">
+                    <p>Record of Grancrest ..</p>
+                </div>
+            </div>
+
+             <div id="coveranime">
+                <img src="/img/animes/capa.jpg">
+                <div id="footera">
+                    <p>One Punch Man</p>
+                </div>
+            </div>
+
+             <div id="coveranime">
+                <img src="/img/animes/rezero.jpg">
+                <div id="footera">
+                    <p>Re:Zero</p>
+                </div>
+            </div>
+
+            <div id="coveranime">
+                <img src="/img/animes/rezero.jpg">
+                <div id="footera">
+                    <p>Re:Zero</p>
+                </div>
+            </div>
+
+             <div id="coveranime">
+                <img src="/img/animes/capa.jpg">
+                <div id="footera">
+                    <p>One Punch Man</p>
+                </div>
+            </div>
         </div>
     </div>
+
+    </div>
+    </div>
+
 @endsection
